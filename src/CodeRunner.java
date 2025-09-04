2f643c8ea48d043195a49781b0df4c4c5920a90c
LucasAHorn <lh.lucashorn@gmail.com>
2025-09-04 16:56:50 -0500

    updating code variables and minor other changes

--- src/CodeRunner.java
+++ src/CodeRunner.java
@@ -21,7 +21,6 @@ public class CodeRunner {
 
         ArrayList<ArrayList<Double>> trainingData = getTrainingData("./Data/weather_cleaned.csv");
         ArrayList<String> filePaths = getRelativeFilePaths("Network_attempt_3/laptop");
-        int roundNum = 0;
 
         try {
 //                Saving logging information into a file
@@ -46,16 +45,30 @@ public class CodeRunner {
 
             System.setOut(teeOut);
 
+            NetworkTrainer[] networkTrainers = new NetworkTrainer[filePaths.size()];
             Thread[] threads = new Thread[filePaths.size()];
 
             for (int i = 0; i < filePaths.size(); i++) {
-                NetworkTrainer nt = new NetworkTrainer(filePaths.get(i), trainingData, 1_000_000, 500_000, 1.5);
-                Thread t = new Thread(nt);
-                t.start();
-                threads[i] = t;
+                networkTrainers[i] = new NetworkTrainer(filePaths.get(i), trainingData, 1_000_000, 10_000, 1.5);
+                Thread thread = new Thread(networkTrainers[i]);
+                thread.start();
+                threads[i] = thread;
             }
             filePaths = null;
 
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                for (NetworkTrainer trainer : networkTrainers) {
+                    trainer.stop();
+                }
+                for (Thread t : threads) {
+                    try {
+                        t.join();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }));
 
 //            to keep the jvm alive
             Thread.currentThread().join();
